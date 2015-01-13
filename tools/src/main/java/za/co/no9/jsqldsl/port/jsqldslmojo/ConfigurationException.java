69cc98aa9d8a913145afddb686e00a072963b59d
Graeme Lockley <graemel@no9.co.za>
2015-01-13 17:32:51 +0200

    Created a single exception to wrap all of the exceptions associated with configuration

---
+++ tools/src/main/java/za/co/no9/jsqldsl/port/jsqldslmojo/ConfigurationException.java
@@ -0,0 +1,7 @@
+package za.co.no9.jsqldsl.port.jsqldslmojo;
+
+public class ConfigurationException extends Exception {
+    public ConfigurationException(String message, Exception ex) {
+        super(message, ex);
+    }
+}
