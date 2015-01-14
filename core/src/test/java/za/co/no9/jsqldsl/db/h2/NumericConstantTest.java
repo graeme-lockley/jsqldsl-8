ccfb4dd79d8068680a8fe5943ecd00dc77b815df
Graeme Lockley <graemel@no9.co.za>
2015-01-14 16:05:58 +0200

    Renamed to be consistent with the NumericConstant class.

--- core/src/test/java/za/co/no9/jsqldsl/db/h2/NumericConstantTest.java
+++ core/src/test/java/za/co/no9/jsqldsl/db/h2/NumericConstantTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertEquals;
 import static za.co.no9.jsqldsl.db.h2.NumericConstant.from;
 import static za.co.no9.jsqldsl.db.h2.Precedence.LOWEST_PRECEDENCE;
 
-public class INTConstantTest {
+public class NumericConstantTest {
     @Test
     public void should_format() throws Exception {
         assertEquals("123", from(123).asString(LOWEST_PRECEDENCE));
