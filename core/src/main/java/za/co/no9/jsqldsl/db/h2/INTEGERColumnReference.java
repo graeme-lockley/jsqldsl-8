f40da9234525dc9d27e3d1d232c213f7fb3dbbe1
Graeme Lockley <graemel@no9.co.za>
2015-01-14 15:50:19 +0200

    Added a from to this class.

--- core/src/main/java/za/co/no9/jsqldsl/db/h2/INTEGERColumnReference.java
+++ core/src/main/java/za/co/no9/jsqldsl/db/h2/INTEGERColumnReference.java
@@ -4,4 +4,8 @@ public class INTEGERColumnReference extends INTColumnReference {
     protected INTEGERColumnReference(TableReference table, String name) {
         super(table, name);
     }
+
+    public static INTEGERColumnReference from(TableReference table, String name) {
+        return new INTEGERColumnReference(table, name);
+    }
 }
