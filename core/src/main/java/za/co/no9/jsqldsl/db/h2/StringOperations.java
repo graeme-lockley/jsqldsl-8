064bc7d679ca39e8bfa66ba037efa9e61df374d5
Graeme Lockley <graemel@no9.co.za>
2015-01-14 15:56:26 +0200

    Added a VARCHAR column reference, a string type and an empty set of string operations.

---
+++ core/src/main/java/za/co/no9/jsqldsl/db/h2/StringOperations.java
@@ -0,0 +1,4 @@
+package za.co.no9.jsqldsl.db.h2;
+
+public abstract class StringOperations implements StringType {
+}
---
+++ core/src/main/java/za/co/no9/jsqldsl/db/h2/VARCHARColumnReference.java
@@ -0,0 +1,20 @@
+package za.co.no9.jsqldsl.db.h2;
+
+public class VARCHARColumnReference extends StringOperations implements ColumnReference, StringType {
+    private final TableReference table;
+    private final String name;
+
+    protected VARCHARColumnReference(TableReference table, String name) {
+        this.table = table;
+        this.name = name;
+    }
+
+    public static VARCHARColumnReference from(TableReference table, String name) {
+        return new VARCHARColumnReference(table, name);
+    }
+
+    @Override
+    public String asString(Precedence precedence) {
+        return table.reference() + '.' + name;
+    }
+}
