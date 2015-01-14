484e84fe6d64f7be32200acd7a26c4367a602a0d
Graeme Lockley <graemel@no9.co.za>
2015-01-14 15:47:38 +0200

    Introduced support for INTEGER

---
+++ core/src/main/java/za/co/no9/jsqldsl/db/h2/INTEGERColumnReference.java
@@ -0,0 +1,7 @@
+package za.co.no9.jsqldsl.db.h2;
+
+public class INTEGERColumnReference extends INTColumnReference {
+    protected INTEGERColumnReference(TableReference table, String name) {
+        super(table, name);
+    }
+}
