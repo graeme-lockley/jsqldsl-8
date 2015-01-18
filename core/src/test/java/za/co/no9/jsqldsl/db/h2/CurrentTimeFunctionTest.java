d0023915737868f96c3cbaf544416ba90f398602
Graeme Lockley <graemel@no9.co.za>
2015-01-18 10:24:52 +0200

    Improved the test to embed the function into a query.

--- core/src/test/java/za/co/no9/jsqldsl/db/h2/CurrentTimeFunctionTest.java
+++ core/src/test/java/za/co/no9/jsqldsl/db/h2/CurrentTimeFunctionTest.java
@@ -2,11 +2,15 @@ package za.co.no9.jsqldsl.db.h2;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static za.co.no9.jsqldsl.db.h2.DSL.CURRENT_TIME;
 
 public class CurrentTimeFunctionTest {
     @Test
-    public void should_marshall_into_a_string() {
-        assertEquals("CURRENT_TIME()", DSL.CURRENT_TIME().asString(Precedence.HIGHEST_PRECEDENCE));
+    public void should_marshal_with_no_arguments() {
+        assertEquals(
+                "FROM TABLE_A AS A WHERE A.TIMESTAMP <= CURRENT_TIME()",
+                Query.from(TABLE_A.as("A"))
+                        .where(a -> a.TIMESTAMP.le(CURRENT_TIME())).asString());
     }
 }
