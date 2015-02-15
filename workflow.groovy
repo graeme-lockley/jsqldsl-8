455201660c76d04165c83d1e027e17b5d202b66b
Graeme Lockley <graemel@no9.co.za>
2015-02-15 06:25:41 +0200

    Fiddling to get script integrated into Jenkins

--- workflow.groovy
+++ workflow.groovy
@@ -1,8 +1,4 @@
-public class Workflow {
-    public void execute() {
-        sh('mvn clean install')
-        step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
-    }
+def execute() {
+    sh('mvn clean install')
+    step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
 }
-
-return new Workflow()
