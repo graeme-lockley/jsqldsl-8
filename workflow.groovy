2617791727c755202520c54b88e0d7e43cdd1b1c
Graeme Lockley <graemel@no9.co.za>
2015-02-15 11:47:35 +0200

    Update sonar stats for this application

--- workflow.groovy
+++ workflow.groovy
@@ -2,6 +2,9 @@ def execute() {
     stage 'Build - clean install'
     sh 'mvn clean install'
     step $class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'
+
+    stage 'Update sonar stats'
+    sh 'mvn -o sonar:sonar'
 }
 
 return this;
