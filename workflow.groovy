8ffd8765097a67fda4a0ed279e120de9b0851510
Graeme Lockley <graemel@no9.co.za>
2015-02-15 05:52:18 +0200

    Made the workflow a set of sets so that the steps are centralised.

--- workflow.groovy
+++ workflow.groovy
@@ -1,5 +1,2 @@
-node {
-  git(url: 'https://github.com/graeme-lockley/jsqldsl-8.git')
-  sh('mvn clean install')
-  step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
-}
+sh('mvn clean install')
+step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
