3a24ef21a6995ebbdf09c2c0f73e8daa49d03404
Graeme Lockley <graemel@no9.co.za>
2015-02-15 07:20:31 +0200

    Added support for SMALLINT

---
+++ core/src/main/java/za/co/no9/jsqldsl/db/h2/SMALLINTColumnReference.java
@@ -0,0 +1,20 @@
+package za.co.no9.jsqldsl.db.h2;
+
+public class SMALLINTColumnReference extends NumericOperations implements ColumnReference, NumericType {
+    private final TableReference table;
+    private final String name;
+
+    protected SMALLINTColumnReference(TableReference table, String name) {
+        this.table = table;
+        this.name = name;
+    }
+
+    public static SMALLINTColumnReference from(TableReference table, String name) {
+        return new SMALLINTColumnReference(table, name);
+    }
+
+    @Override
+    public String asString(Precedence precedence) {
+        return table.reference() + '.' + name;
+    }
+}
