932a3ca24368dc62bb41c6961331a3d2d9ae0aef
Graeme Lockley <graemel@no9.co.za>
2015-02-15 07:25:30 +0200

    Made from public

--- core/src/main/java/za/co/no9/jsqldsl/db/h2/Query.java
+++ core/src/main/java/za/co/no9/jsqldsl/db/h2/Query.java
@@ -1,7 +1,7 @@
 package za.co.no9.jsqldsl.db.h2;
 
 public class Query {
-    static <T1 extends TableReference> Query1<T1> from(T1 tableRef) {
+    public static <T1 extends TableReference> Query1<T1> from(T1 tableRef) {
         return new Query1<T1>(tableRef);
     }
 }
