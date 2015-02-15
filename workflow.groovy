873d79ebafb75096ff577a7b098020d289f99537
Graeme Lockley <graemel@no9.co.za>
2015-02-15 06:39:41 +0200

    Simplified workflow

--- workflow.groovy
+++ workflow.groovy
@@ -1,7 +1,7 @@
 def execute() {
-    sh('mvn clean install')
-    step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
+    stage 'Build - clean install'
+    sh 'mvn clean install'
+    step $class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'
 }
 
-return this
-
+return this;
