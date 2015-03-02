e6bbd08bab98f27d8d306a40958922ea3569de6e
Graeme Lockley <graemel@no9.co.za>
2015-03-02 21:36:03 +0200

    Moved the ascertaining of what fields are within a table out of TableMetaData's from into H2.  I can then extract most of this behavior into a common parent allowing to specialise for different database types.

--- tools/src/main/java/za/co/no9/jsqldsl/drivers/H2.java
+++ tools/src/main/java/za/co/no9/jsqldsl/drivers/H2.java
@@ -1,10 +1,13 @@
 package za.co.no9.jsqldsl.drivers;
 
+import org.apache.commons.lang3.StringUtils;
 import za.co.no9.jsqldsl.tools.*;
 
 import java.io.File;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.*;
 
 public class H2 implements DBDriver {
     @Override
@@ -14,7 +17,54 @@ public class H2 implements DBDriver {
 
     @Override
     public TableMetaData tableMetaData(Connection connection, TableName tableName) throws SQLException {
-        return TableMetaData.from(connection, tableName);
+        Set<String> primaryKey = primaryKey(connection, tableName);
+
+        return new TableMetaData(tableName, fields(connection, tableName, primaryKey));
+    }
+
+    protected Set<String> primaryKey(Connection connection, TableName tableName) throws SQLException {
+        java.sql.DatabaseMetaData dbm = connection.getMetaData();
+
+        Set<String> primaryKey = new HashSet<>();
+        try (ResultSet resultSet = dbm.getPrimaryKeys("", "", tableName.dbName())) {
+            while (resultSet.next()) {
+                primaryKey.add(resultSet.getString(4));
+            }
+        }
+
+        return primaryKey;
+    }
+
+    private FieldMetaData[] fields(Connection connection, TableName tableName, Set<String> primaryKeys) throws SQLException {
+        java.sql.DatabaseMetaData dbm = connection.getMetaData();
+
+        List<FieldMetaData> fields = new ArrayList<>();
+        try (ResultSet resultSet = dbm.getColumns(tableName.catalog().orElse(""), tableName.schema().orElse(""), tableName.dbName(), null)) {
+            while (resultSet.next()) {
+                fields.add(fromColumnsResultSet(primaryKeys, resultSet));
+            }
+        }
+
+        return fields.toArray(new FieldMetaData[1]);
+    }
+
+    protected FieldMetaData fromColumnsResultSet(Set<String> primaryKeys, ResultSet resultSet) throws SQLException {
+        String fieldName = resultSet.getString(4);
+        String fieldType = resultSet.getString(6);
+        Optional<Integer> columnSize = parseInt(resultSet.getString(7));
+        Optional<Integer> subWidth = parseInt(resultSet.getString(9));
+        boolean nullable = Integer.parseInt(resultSet.getString(11)) == 1;
+        boolean isAutoIncrement = StringUtils.equalsIgnoreCase("YES", resultSet.getString(23));
+
+        return new FieldMetaData(fieldName, fieldType, columnSize, subWidth, nullable, primaryKeys.contains(fieldName), isAutoIncrement);
+    }
+
+    private static Optional<Integer> parseInt(String intString) {
+        try {
+            return Optional.of(Integer.parseInt(intString));
+        } catch (NullPointerException | NumberFormatException ignored) {
+            return Optional.empty();
+        }
     }
 
     @Override
--- tools/src/main/java/za/co/no9/jsqldsl/tools/TableMetaData.java
+++ tools/src/main/java/za/co/no9/jsqldsl/tools/TableMetaData.java
@@ -1,71 +1,17 @@
 package za.co.no9.jsqldsl.tools;
 
-import org.apache.commons.lang3.StringUtils;
-
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class TableMetaData {
-    private TableName tableName;
-    private FieldMetaData[] fieldsMetaData;
+    private final TableName tableName;
+    private final FieldMetaData[] fieldsMetaData;
 
-    private TableMetaData(TableName tableName, FieldMetaData[] fieldsMetaData) {
+    public TableMetaData(TableName tableName, FieldMetaData[] fieldsMetaData) {
         this.tableName = tableName;
         this.fieldsMetaData = fieldsMetaData;
-    }
-
-    public static TableMetaData from(Connection connection, TableName tableName) throws SQLException {
-        Set<String> primaryKey = primaryKey(connection, tableName);
-
-        return new TableMetaData(tableName, fields(connection, tableName, primaryKey));
-    }
-
-    private static Set<String> primaryKey(Connection connection, TableName tableName) throws SQLException {
-        DatabaseMetaData dbm = connection.getMetaData();
-
-        Set<String> primaryKey = new HashSet<>();
-        try (ResultSet resultSet = dbm.getPrimaryKeys("", "", tableName.dbName())) {
-            while (resultSet.next()) {
-                primaryKey.add(resultSet.getString(4));
-            }
-        }
-
-        return primaryKey;
-    }
-
-    private static FieldMetaData[] fields(Connection connection, TableName tableName, Set<String> primaryKeys) throws SQLException {
-        DatabaseMetaData dbm = connection.getMetaData();
-
-        List<FieldMetaData> fields = new ArrayList<>();
-        try (ResultSet resultSet = dbm.getColumns(tableName.catalog().orElse(""), tableName.schema().orElse(""), tableName.dbName(), null)) {
-            while (resultSet.next()) {
-                fields.add(fromColumnsResultSet(primaryKeys, resultSet));
-            }
-        }
-
-        return fields.toArray(new FieldMetaData[1]);
-    }
-
-    protected static FieldMetaData fromColumnsResultSet(Set<String> primaryKeys, ResultSet resultSet) throws SQLException {
-        String fieldName = resultSet.getString(4);
-        String fieldType = resultSet.getString(6);
-        Optional<Integer> columnSize = parseInt(resultSet.getString(7));
-        Optional<Integer> subWidth = parseInt(resultSet.getString(9));
-        boolean nullable = Integer.parseInt(resultSet.getString(11)) == 1;
-        boolean isAutoIncrement = StringUtils.equalsIgnoreCase("YES", resultSet.getString(23));
-
-        return new FieldMetaData(fieldName, fieldType, columnSize, subWidth, nullable, primaryKeys.contains(fieldName), isAutoIncrement);
-    }
-
-    private static Optional<Integer> parseInt(String intString) {
-        try {
-            return Optional.of(Integer.parseInt(intString));
-        } catch (NullPointerException | NumberFormatException ignored) {
-            return Optional.empty();
-        }
     }
 
     public TableName tableName() {
