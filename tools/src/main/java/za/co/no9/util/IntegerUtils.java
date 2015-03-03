ff08ac6ed55e7103a6a12b834659e04d0d822875
Graeme Lockley <graemel@no9.co.za>
2015-03-03 21:58:17 +0200

    Made the class final and added a private constructor.

--- tools/src/main/java/za/co/no9/util/IntegerUtils.java
+++ tools/src/main/java/za/co/no9/util/IntegerUtils.java
@@ -2,7 +2,10 @@ package za.co.no9.util;
 
 import java.util.Optional;
 
-public class IntegerUtils {
+public final class IntegerUtils {
+    private IntegerUtils() {
+    }
+
     public static Optional<Integer> parseInt(String intString) {
         try {
             return Optional.of(Integer.parseInt(intString));
