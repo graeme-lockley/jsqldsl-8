9e44fc428a59187081c458034dd80018d22b52cd
Graeme Lockley <graemel@no9.co.za>
2015-04-03 11:44:04 +0200

    Changed the internal structure of allTables to create a map rather than a list.  This will allow constraints to be cleaned up to refer to FieldMetaData rather than a string for the column name.

--- tools/src/main/java/za/co/no9/jsqldsl/tools/DatabaseMetaData.java
+++ tools/src/main/java/za/co/no9/jsqldsl/tools/DatabaseMetaData.java
@@ -5,8 +5,9 @@ import za.co.no9.jsqldsl.drivers.DBDriver;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DatabaseMetaData {
     private DBDriver dbDriver;
@@ -21,20 +22,21 @@ public class DatabaseMetaData {
         return new DatabaseMetaData(dbDriver, connection);
     }
 
-    public List<TableMetaData> allTables() throws SQLException {
+    public Collection<TableMetaData> allTables() throws SQLException {
         return tables(null, null);
     }
 
-    private List<TableMetaData> tables(String schemaNamePattern, String tableNamePattern) throws SQLException {
-        List<TableMetaData> tables = new ArrayList<>();
+    private Collection<TableMetaData> tables(String schemaNamePattern, String tableNamePattern) throws SQLException {
+        Map<TableName, TableMetaData> tables = new HashMap<>();
 
         java.sql.DatabaseMetaData metaData = connection.getMetaData();
         try (ResultSet rs = metaData.getTables(null, schemaNamePattern, tableNamePattern, null)) {
             while (rs.next()) {
-                tables.add(dbDriver.tableMetaData(connection, TableName.from(rs.getString(1), rs.getString(2), rs.getString(3))));
+                TableMetaData tableMetaData = dbDriver.tableMetaData(connection, TableName.from(rs.getString(1), rs.getString(2), rs.getString(3)));
+                tables.put(tableMetaData.tableName(), tableMetaData);
             }
         }
 
-        return tables;
+        return tables.values();
     }
 }
