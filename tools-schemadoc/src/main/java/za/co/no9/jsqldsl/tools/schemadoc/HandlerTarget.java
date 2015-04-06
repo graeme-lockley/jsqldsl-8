3ef17d5abd265ee738c1db4600aa83aef443862e
Graeme Lockley <graemel@no9.co.za>
2015-04-06 08:54:17 +0200

    Moved the schemadoc template in as resources - this makes the usage easier for first time users.

--- tools-schemadoc/src/main/java/za/co/no9/jsqldsl/tools/schemadoc/HandlerTarget.java
+++ tools-schemadoc/src/main/java/za/co/no9/jsqldsl/tools/schemadoc/HandlerTarget.java
@@ -1,9 +1,18 @@
 package za.co.no9.jsqldsl.tools.schemadoc;
 
+import org.apache.commons.io.IOUtils;
 import za.co.no9.jsqldsl.port.jsqldslmojo.Target;
 import za.co.no9.jsqldsl.tools.HandlerTargetParent;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class HandlerTarget extends HandlerTargetParent {
+    private static final String DEFAULT_RESOURCE = "";
+    private static final String START_DOCUMENT_TEMPLATE_RESOURCE = "schemadoc/start-document-template.ftl";
+    private static final String TABLE_TEMPLATE_RESOURCE = "schemadoc/table-template.ftl";
+    private static final String END_DOCUMENT_TEMPLATE_RESOURCE = "schemadoc/end-document-template.ftl";
+
     protected HandlerTarget(Target target) {
         super(target);
     }
@@ -13,14 +22,24 @@ public class HandlerTarget extends HandlerTargetParent {
     }
 
     public String startDocumentTemplate() {
-        return target.getProperty("start-document-template").orElse("").trim();
+        return target.getProperty("start-document-template").orElse(loadResource(START_DOCUMENT_TEMPLATE_RESOURCE)).trim();
     }
 
     public String tableTemplate() {
-        return target.getProperty("table-template").orElse("").trim();
+        return target.getProperty("table-template").orElse(loadResource(TABLE_TEMPLATE_RESOURCE)).trim();
     }
 
     public String endDocumentTemplate() {
-        return target.getProperty("end-document-template").orElse("").trim();
+        return target.getProperty("end-document-template").orElse(loadResource(END_DOCUMENT_TEMPLATE_RESOURCE)).trim();
+    }
+
+    private String loadResource(String resourceName) {
+        ClassLoader loader = getClass().getClassLoader();
+
+        try (InputStream is = loader.getResourceAsStream(resourceName)) {
+            return IOUtils.toString(is);
+        } catch (IOException e) {
+            return DEFAULT_RESOURCE;
+        }
     }
 }
---
+++ tools-schemadoc/src/main/resources/schemadoc/start-document-template.ftl
@@ -0,0 +1,11 @@
+digraph G {
+    node [
+        fontname="Helvetica"
+        fontsize="11"
+        shape="plaintext"
+    ];
+    edge [
+        arrowsize="0.8"
+        fontname="Helvetica"
+        fontsize="8"
+    ];
